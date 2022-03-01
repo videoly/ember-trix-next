015e7864fe1b122bcf60f7024b1ae0202a4a9276
Oleg Larkin <oleg.larkin@nordigy.ru>
2022-03-01 20:36:57 +0300

    chore: demo deploy fix

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'dummy',
     environment,
-    rootURL: '/',
+    rootURL: '/ember-trix-next/',
     locationType: 'history',
     EmberENV: {
       FEATURES: {
